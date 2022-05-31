659e1d47fdfa8604edb310ccd6516b3f31d470b6
MMichels <michels09@hotmail.com>
2022-05-30 22:48:53 -0300

    feat(Screenshot): Lib e funcão de screenshot ok

--- src/components/Widget/ScreenshotButton/index.tsx
+++ src/components/Widget/ScreenshotButton/index.tsx
@@ -1,18 +1,19 @@
 import { Camera, Trash } from 'phosphor-react-native';
 import React from 'react';
-import { View, TouchableOpacity, Image } from 'react-native';
+import { View, TouchableOpacity, Image, ActivityIndicator } from 'react-native';
 import { theme } from '../../../theme';
 
 import { styles } from './styles';
 
 interface ScreenshotProps {
     screenshot: string | null;
+    isTakingScreenshot: boolean;
     onTakeShot: (screenshot: string) => void;
     onRemoveShot: () => void;
 }
 
 
-export function ScreenshotButton({screenshot, onTakeShot, onRemoveShot}: ScreenshotProps) {
+export function ScreenshotButton({screenshot, isTakingScreenshot, onTakeShot, onRemoveShot}: ScreenshotProps) {
     function takeScreenShot(){
         onTakeShot("blsabslb");
     }
@@ -22,22 +23,26 @@ export function ScreenshotButton({screenshot, onTakeShot, onRemoveShot}: Screens
         onPress={screenshot ? onRemoveShot : takeScreenShot}    
     >
         {
-            screenshot ? 
-                <View>
-                    <Image 
-                        style={styles.image}
-                        source={{uri: screenshot}}
+            isTakingScreenshot ? 
+            <ActivityIndicator 
+                color={theme.colors.text_on_brand_color}
+            /> :
+                screenshot ? 
+                    <View>
+                        <Image 
+                            style={styles.image}
+                            source={{uri: screenshot}}
+                        />
+                        <Trash 
+                        style={styles.removeIcon} 
+                        size={22} 
+                        color={theme.colors.text_secondary}
+                    /> 
+                    </View>:
+                    <Camera
+                        size={24} weight='bold'
+                        color={theme.colors.text_primary}
                     />
-                    <Trash 
-                    style={styles.removeIcon} 
-                    size={22} 
-                    color={theme.colors.text_secondary}
-                /> 
-                </View>:
-                <Camera
-                    size={24} weight='bold'
-                    color={theme.colors.text_primary}
-                />
         }
 
     </TouchableOpacity>
